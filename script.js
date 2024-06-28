d1239594586f999432d3fdc7bff52fd2c3663f85
Nika <Nika@users-MacBook-Pro.local>
2024-06-28 17:27:06 +0400

    feat: toggle temperatures
    refactor code, destructure filtered weather in displayWeather function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const errorSpan = document.querySelector(".error-message");
+let filteredWeatherData = null;
 
 async function fetchWeather(city) {
   try {
@@ -9,26 +10,8 @@ async function fetchWeather(city) {
       }`
     );
     const weatherData = await response.json();
-    const {
-      location,
-      condition,
-      temp_c,
-      temp_f,
-      feelslike_c,
-      feelslike_f,
-      wind,
-      humidity,
-      locationCountry,
-    } = filterWeather(weatherData);
-    displayWeather(
-      condition,
-      location,
-      temp_c,
-      feelslike_c,
-      wind,
-      humidity,
-      locationCountry
-    );
+    filteredWeatherData = filterWeather(weatherData);
+    displayWeather(filteredWeatherData);
   } catch (err) {
     console.error(err);
     errorSpan.innerHTML = "Location not found";
@@ -68,18 +51,54 @@ document.querySelector("#weather-form").addEventListener("submit", (e) => {
   fetchWeather(e.target.children[1].value);
 });
 
-function displayWeather(condition, city, temp, feels, wind, humidity, country) {
+function displayWeather(filteredWeatherData) {
+  const {
+    condition,
+    location: city,
+    temp_c,
+    temp_f,
+    feelslike_c,
+    feelslike_f,
+    wind,
+    humidity,
+    locationCountry: country,
+  } = filteredWeatherData;
+
   const connditionEl = document.querySelector(".condition");
   const cityEl = document.querySelector(".city");
   const tempEl = document.querySelector(".temp");
   const feelsEl = document.querySelector(".feels");
   const windEl = document.querySelector(".wind");
   const humidityEl = document.querySelector(".humidity");
+  const toggleButton = document.querySelector(".toggle-button");
 
   connditionEl.innerHTML = condition;
   cityEl.innerHTML = `${city}, ${country}`;
-  tempEl.innerHTML = `${Math.round(temp)}&deg;C `;
-  feelsEl.innerHTML = `Feels Like: ${feels}&deg;C`;
+
+  if (toggleButton.classList.contains("fa-toggle-off")) {
+    tempEl.innerHTML = `${Math.round(temp_c)}&deg;C `;
+    feelsEl.innerHTML = `Feels Like: ${feelslike_c}&deg;C`;
+  } else if (toggleButton.classList.contains("fa-toggle-on")) {
+    tempEl.innerHTML = `${Math.round(temp_f)}&deg;F `;
+    feelsEl.innerHTML = `Feels Like: ${feelslike_f}&deg;F`;
+  }
   windEl.innerHTML = `Wind: ${wind} KPH`;
   humidityEl.innerHTML = `Humidity: ${humidity}%`;
 }
+
+function toggleTemperature() {
+  const toggleOn = document.querySelector(".fa-toggle-on");
+  const toggleOff = document.querySelector(".fa-toggle-off");
+  if (toggleOn) {
+    toggleOn.classList = `fa-solid fa-toggle-off toggle-button`;
+  } else if (toggleOff) {
+    toggleOff.classList = `fa-solid fa-toggle-on toggle-button`;
+  }
+
+  if (filteredWeatherData) {
+    displayWeather(filteredWeatherData);
+  }
+}
+
+const toggleButton = document.querySelector(".toggle-button");
+toggleButton.addEventListener("click", toggleTemperature);
