c7c71527c1656c0aa5bd24df03d517aeaf233980
Nika <Nika@users-MacBook-Pro.local>
2024-06-28 13:42:19 +0400

    style: misspelled function name

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-async function fetchWeatehr() {
+async function fetchWeather() {
   try {
     const response = await fetch(
       "https://api.weatherapi.com/v1/current.json?key=772983b77eef43edb39103859242706&q=London"
     );
     const weatherData = await response.json();
+    filterWeather(weatherData);
   } catch (err) {
     console.log(err);
   }
@@ -12,3 +13,5 @@ async function fetchWeatehr() {
 function filterWeather(data) {
   console.log(data);
 }
+
+fetchWeather();
