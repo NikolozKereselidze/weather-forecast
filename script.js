8b82165e8f5e7b189424af4e9adbb515f1d2272a
Nika <Nika@users-MacBook-Pro.local>
2024-06-28 13:40:19 +0400

    feat: add try/catch block to the weather fetch function

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 async function fetchWeatehr() {
-  const response = await fetch(
-    "https://api.weatherapi.com/v1/current.json?key=772983b77eef43edb39103859242706&q=London"
-  );
-
-  const weatherData = await response.json();
+  try {
+    const response = await fetch(
+      "https://api.weatherapi.com/v1/current.json?key=772983b77eef43edb39103859242706&q=London"
+    );
+    const weatherData = await response.json();
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 function filterWeather(data) {
