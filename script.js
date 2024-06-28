fc1972a568ce1ae3eb1d96db6fcbd2c958ba9f82
Nika <Nika@users-MacBook-Pro.local>
2024-06-28 14:07:23 +0400

    feat: create a function to filter and destructure weather data

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 async function fetchWeather() {
   try {
     const response = await fetch(
-      "https://api.weatherapi.com/v1/current.json?key=772983b77eef43edb39103859242706&q=London"
+      "https://api.weatherapi.com/v1/current.json?key=772983b77eef43edb39103859242706&q=Tbilisi"
     );
     const weatherData = await response.json();
-    filterWeather(weatherData);
+    const filteredData = filterWeather(weatherData);
+    console.log(filteredData);
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 }
 
-function filterWeather(data) {
-  console.log(data);
+function filterWeather({ current, location }) {
+  const {
+    condition: { text: condition },
+    temp_c,
+    temp_f,
+    feelslike_c,
+    feelslike_f,
+    wind_kph: wind,
+    humidity,
+  } = current;
+
+  const { name: locationName } = location;
+
+  return {
+    location: locationName,
+    condition,
+    temp_c,
+    temp_f,
+    feelslike_c,
+    feelslike_f,
+    wind,
+    humidity,
+  };
 }
 
 fetchWeather();
